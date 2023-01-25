52dfaab03442c208a0d94c46cdb1ccfe492ec3d2
AleJcA <alexander18072003m@gmail.com>
2023-01-24 21:02:09 -0600

    Commit Summary

---
+++ Ejersicio 1 Semana 2/Ejersicio 1 Semana 2/Modelos/Carro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Ejersicio_1_Semana_2.Modelos
+{
+    internal class Carro
+    {
+        public string placa { get; set; }
+        public string color { get; set; }
+
+        public string marca { get; set; }
+        public int anioProduccion { get; set; }
+        public int kms  { get; set; }
+
+        public string toString()
+        {
+
+            return $"Placa:  {placa} -  Color: {color} - Marca {marca} - Año de produccion: {anioProduccion} - Kms recorridos {kms}";
+
+        }
+
+    }
+}
--- Ejersicio 1 Semana 2/Ejersicio 1 Semana 2/Program.cs
+++ Ejersicio 1 Semana 2/Ejersicio 1 Semana 2/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+using Ejersicio_1_Semana_2.Modelos;
+
+List<Carro> Carros = new List<Carro>();
+
+Carro C1 = new Carro()
+{
+
+    placa = "HAG603",
+    color = "Rojo",
+    marca = "BMW",
+    anioProduccion = 1991,
+    kms = 0
+
+};
+
+Carros.Add(C1);
+
+foreach (Carro x in Carros)
+{
+    Console.WriteLine(x.toString());
+}
+
